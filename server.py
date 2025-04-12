d56394d8210df7a252d86b9a5071f96817987948
Andre.Balakirev <andre.balakirev@mail.ru>
2025-04-12 10:08:33 +0300

    изменил стуктуру шаблона

--- server.py
+++ server.py
@@ -1,25 +1,15 @@
 from flask import Flask
 from weather import weather_by_city
+from flask import render_template
+import requests
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    weather = weather_by_city("Moscow,Russia")
-    if weather:
-       weather_text = f"Сейчас {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
-    else:
-       weather_text = "Прогноз сейчас недоступен"
-
-    return f"""<html>
-    <head>
-        <title>Прогноз погоды</title>
-        </head>
-        <body>
-        <h1>{weather_text}</h1>
-    </body>
-</html>"""
-
+    data = weather_by_city("Moscow,Russia")
+    return render_template('index.html', weather=data)
+    
 
 if __name__=="__main__":
     app.run()
